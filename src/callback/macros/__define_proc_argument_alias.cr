c97f8ffe1655b09cfd63df5d6b6f025578ed246c
mosop <mosop@users.noreply.github.com>
2016-12-04 18:13:05 +0900

    fix

--- src/callback/macros/__define_proc_argument_alias.cr
+++ src/callback/macros/__define_proc_argument_alias.cr
@@ -2,7 +2,11 @@ module Callback
   macro __define_proc_argument_alias(prefix, suffix, inherit, *types)
     {% for e, i in types %}
     # :nodoc:
-    alias {{prefix.id}}ProcArgumentType{{suffix.id}}{{i+1}} = {{types[i].id}}
+      {% if i == 0 %}
+        alias {{prefix.id}}ProcArgumentType{{suffix.id}}{{i+1}} = ::{{types[i].id}}
+      {% else %}
+        alias {{prefix.id}}ProcArgumentType{{suffix.id}}{{i+1}} = {{types[i].id}}
+      {% end %}
     {% end %}
   end
 end
